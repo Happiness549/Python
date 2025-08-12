64c838ce7be8d591a4ed2dd4673ed26b46b0b4ea
Happiness Mhlongo <mhlongohappiness880@gmail.com>
2025-08-12 10:09:55 +0200

    Added week2.py file

---
+++ week2.py
@@ -0,0 +1,21 @@
+my_list = []
+
+my_list.append(10)
+my_list.appne(20)
+my_list.append(30)
+my_list.appned(40)
+
+my_list.insert(1, 15)
+
+my_list.extend([50,60,70])
+
+del my_list[6]
+
+my_list.pop()
+
+my_list.sort()
+
+index_30 =my_list.index(30)
+print("Index of 30:", index_30)
+
+print("Finale list:", my_list)
